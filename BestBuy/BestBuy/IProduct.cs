fa6f3c117090c33716326b130afe549c17967ff6
John Clark <bennyclarkup@gmail.com>
2019-09-26 10:36:18 -0500

    Framework

---
+++ BestBuy/BestBuy/IProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BestBuy
+{
+    public interface IProduct
+    {
+       string Name { get; set; }
+       double Price { get; set; }
+       double PriceWithTax();
+       
+
+    }
+
+}
---
+++ BestBuy/BestBuy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BestBuy
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Product cork = new Product();
+            cork.Price = .10 ;
+            cork.Name = "Brewing Corks ";
+
+            double pricetax =  cork.PriceWithTax();
+
+            Console.WriteLine(cork.Price + " "+ cork.Name + pricetax);
+        }
+        
+    }
+}
